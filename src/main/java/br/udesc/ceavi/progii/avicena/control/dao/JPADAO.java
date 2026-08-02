a13c9950d40ae6c83c256071d038922450eb0e59
Mario Fronza <mariofronza1@gmail.com>
2026-08-02 14:55:34 -0300

    fix JPADAO closing its EntityManager after the first operation
    Previously JPADAO held a static EntityManagerFactory/EntityManager pair
    created once in the constructor, with inserir() closing the shared
    EntityManager in a finally block. Any second operation on the same
    instance (or any instance, since the fields were static) then failed
    with IllegalStateException: The EntityManager has been closed.
    
    Now the EntityManagerFactory is a single shared instance, and each
    operation opens and closes its own EntityManager.

--- src/main/java/br/udesc/ceavi/progii/avicena/control/dao/JPADAO.java
+++ src/main/java/br/udesc/ceavi/progii/avicena/control/dao/JPADAO.java
@@ -17,84 +17,78 @@ import javax.persistence.Query;
  * @version 1.0
  */
 public class JPADAO<X> implements DAO{
-    private static EntityManagerFactory emf;
-    private static EntityManager em;
+    private static final EntityManagerFactory emf = PersistenceConfig.createEntityManagerFactory();
 
-    public JPADAO() {
-        super();
-        emf = javax.persistence.Persistence.createEntityManagerFactory("AvicenaBD");
-        em = emf.createEntityManager();
-    }
-    
-    
-    
     @Override
     public boolean inserir(Object obj) throws Exception {
-        em.getTransaction().begin();
-        
+        EntityManager em = emf.createEntityManager();
         try {
+            em.getTransaction().begin();
             em.persist(obj);
             em.getTransaction().commit();
-            
+
             return true;
         } catch (Exception e) {
             System.out.println(e.getMessage());
-//            e.printStackTrace();
             em.getTransaction().rollback();
         } finally{
             em.close();
         }
-        
+
         return false;
     }
 
     @Override
     public boolean atualizar(Object obj) throws Exception {
-        em.getTransaction().begin();
+        EntityManager em = emf.createEntityManager();
         try {
+            em.getTransaction().begin();
             em.merge(obj);
             em.getTransaction().commit();
-            
+
             return true;
         } catch (Exception e) {
             e.printStackTrace();
             em.getTransaction().rollback();
+        } finally {
+            em.close();
         }
-        
+
         return false;
     }
 
     @Override
     public boolean deletar(Object obj) throws Exception {
-        em.getTransaction().begin();
+        EntityManager em = emf.createEntityManager();
         try {
+            em.getTransaction().begin();
             em.remove(obj);
             em.getTransaction().commit();
-            
+
             return true;
         } catch (Exception e) {
             e.printStackTrace();
             em.getTransaction().rollback();
+        } finally {
+            em.close();
         }
-        
+
         return false;
     }
 
     @Override
     public Object pesquisarPorId(Class tipo, long id) throws Exception {
+        EntityManager em = emf.createEntityManager();
         Object object = null;
-        em.getTransaction().begin();
-        
+
         try {
             object = em.find(tipo, id);
-            
-            
-            em.getTransaction().commit();
         } catch (Exception e) {
             e.printStackTrace();
-            em.getTransaction().rollback();
+        } finally {
+            em.close();
         }
-        
+
         return object;
     }
 
