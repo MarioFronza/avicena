c24c1cbfdefd9dbfa144c961749df0a04e2281ce
Mario Fronza <mariofronza1@gmail.com>
2026-08-04 21:57:02 -0300

    test: cover Doctor field exposure after valid construction

--- src/test/java/br/udesc/ceavi/progii/avicena/doctor/domain/DoctorTest.java
+++ src/test/java/br/udesc/ceavi/progii/avicena/doctor/domain/DoctorTest.java
@@ -1,7 +1,9 @@
 package br.udesc.ceavi.progii.avicena.doctor.domain;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import br.udesc.ceavi.progii.avicena.patient.domain.Address;
 import br.udesc.ceavi.progii.avicena.patient.domain.MaritalStatus;
 import org.junit.jupiter.api.Test;
 
@@ -13,5 +15,21 @@ class DoctorTest {
                 InvalidDoctorDataException.class,
                 () -> new Doctor(
                         "", "12345678900", "48999990000", null, MaritalStatus.SINGLE, "12345", "Cardiology"));
+    }
+
+    @Test
+    void exposesFieldsAfterValidConstruction() {
+        Address address = new Address(100, "Apt 2", "88000000", "Main St", "Downtown", "Florianopolis");
+
+        Doctor doctor = new Doctor(
+                "Joao Teste", "12345678900", "48999990000", address, MaritalStatus.SINGLE, "12345", "Cardiology");
+
+        assertEquals("Joao Teste", doctor.getName());
+        assertEquals("12345678900", doctor.getCpf());
+        assertEquals("48999990000", doctor.getPhone());
+        assertEquals(address, doctor.getAddress());
+        assertEquals(MaritalStatus.SINGLE, doctor.getMaritalStatus());
+        assertEquals("12345", doctor.getCrm());
+        assertEquals("Cardiology", doctor.getSpecialty());
     }
 }
