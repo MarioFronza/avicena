1f55aeab2c96db84f732789abf628e7eb2d9aca2
Mario Fronza <mariofronza1@gmail.com>
2026-08-05 11:55:18 -0300

    test: verify Nurse exposes all fields after valid construction

--- src/test/java/br/udesc/ceavi/progii/avicena/nurse/domain/NurseTest.java
+++ src/test/java/br/udesc/ceavi/progii/avicena/nurse/domain/NurseTest.java
@@ -1,7 +1,9 @@
 package br.udesc.ceavi.progii.avicena.nurse.domain;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import br.udesc.ceavi.progii.avicena.patient.domain.Address;
 import br.udesc.ceavi.progii.avicena.patient.domain.MaritalStatus;
 import org.junit.jupiter.api.Test;
 
@@ -12,5 +14,21 @@ class NurseTest {
         assertThrows(
                 InvalidNurseDataException.class,
                 () -> new Nurse("", "12345678900", "48999990000", null, MaritalStatus.SINGLE, "Tecnico", 1200));
+    }
+
+    @Test
+    void exposesFieldsAfterValidConstruction() {
+        Address address = new Address(100, "Apt 2", "88000000", "Main St", "Downtown", "Florianopolis");
+
+        Nurse nurse = new Nurse(
+                "Joana Teste", "12345678900", "48999990000", address, MaritalStatus.SINGLE, "Tecnico", 1200);
+
+        assertEquals("Joana Teste", nurse.getName());
+        assertEquals("12345678900", nurse.getCpf());
+        assertEquals("48999990000", nurse.getPhone());
+        assertEquals(address, nurse.getAddress());
+        assertEquals(MaritalStatus.SINGLE, nurse.getMaritalStatus());
+        assertEquals("Tecnico", nurse.getFormation());
+        assertEquals(1200, nurse.getHoursCompleted());
     }
 }
