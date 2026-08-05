c5ab0d878caed29a41f34d3b729079eee3164ff8
Mario Fronza <mariofronza1@gmail.com>
2026-08-05 16:49:55 -0300

    fix: cascade PERSIST on PatientEntity.address to fix save with a fresh address

--- src/main/java/br/udesc/ceavi/progii/avicena/patient/infrastructure/persistence/PatientEntity.java
+++ src/main/java/br/udesc/ceavi/progii/avicena/patient/infrastructure/persistence/PatientEntity.java
@@ -1,6 +1,7 @@
 package br.udesc.ceavi.progii.avicena.patient.infrastructure.persistence;
 
 import br.udesc.ceavi.progii.avicena.patient.domain.MaritalStatus;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -28,7 +29,7 @@ public class PatientEntity {
     @Column(name = "telefone", nullable = false)
     private String phone;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "codigo_endereco")
     private AddressEntity address;
 
