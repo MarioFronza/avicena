f385b688d1860cc3a301b6be21a5d2a90543f9fe
Mario Fronza <mariofronza1@gmail.com>
2026-08-05 14:28:35 -0300

    refactor: consolidate exceptions into progii package, drop unused CadastroNaoEncontradoException

--- src/main/java/br/udesc/ceavi/progii/avicena/control/exceptions/ValorNuloException.java
+++ src/main/java/br/udesc/ceavi/progii/avicena/control/exceptions/ValorNuloException.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.udesc.ceavi.avicena.control.exceptions;
+package br.udesc.ceavi.progii.avicena.control.exceptions;
 
 /**
  * Classe responsavel pela exceção de um valor nulo
